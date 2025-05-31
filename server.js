3d18d844b9d2e7e9aec4226300e4b713e01b1494
ISTERQ <131639934+ISTERQ@users.noreply.github.com>
2025-05-31 19:34:40 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const path = require('path');
 
